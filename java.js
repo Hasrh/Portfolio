04f8fc812c6d5ab19d7800f89ac729ecb3c524e5
harsh <58473933+Hasrh@users.noreply.github.com>
2024-04-19 21:46:07 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+const navbarLinks = document.querySelectorAll('.topnav a');
+
+navbarLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const href = event.target.getAttribute('href');
+        const section = document.querySelector(href);
+
+        section.scrollIntoView({ behavior: 'smooth' });
+    });
+});
